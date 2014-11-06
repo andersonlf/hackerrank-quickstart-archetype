8fd0e82562ad4b8ef07e62876905d5e3a175b119
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-11-06 14:20:03 -0200

    Good ideas for archetype. Thank you Odilon.

--- src/main/resources/archetype-resources/src/main/java/Solution.java
+++ src/main/resources/archetype-resources/src/main/java/Solution.java
@@ -4,6 +4,11 @@ public class Solution {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		solve(in);
 		in.close();
+	}
+
+	public static void solve(Scanner in) {
+		// your code must be here
 	}
 }
--- src/main/resources/archetype-resources/src/test/java/SolutionTest.java
+++ src/main/resources/archetype-resources/src/test/java/SolutionTest.java
@@ -1,10 +1,40 @@
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SolutionTest {
 
+	private final OutputStream out = new ByteArrayOutputStream();
+	
+	@Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
 	@Test
 	public void test1() throws Exception {
-		Assert.fail("not implemented yet");
+		Scanner in = new Scanner(new File("src/test/resources/test1/in.dat"));
+		String expectedOutput = getExpectedOutput("src/test/resources/test1/out.dat");
+		Solution.solve(in);
+		Assert.assertEquals(expectedOutput, out.toString());
+	}
+
+	private String getExpectedOutput(String string) throws IOException {
+		return new String(Files.readAllBytes(Paths.get(string))) + "\n";
 	}
 }
